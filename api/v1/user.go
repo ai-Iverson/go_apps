5c0b0303eea6888a0863d70a31bbbf7aa8140d1b
张新 <zhangxin@zhangxindeMacBook-Pro.local>
2022-08-09 21:38:52 +0800

    test3

--- api/v1/user.go
+++ api/v1/user.go
@@ -15,13 +15,3 @@ type UserRegisterReq struct {
 type UserRegisterRes struct {
 	Msg string
 }
-type UsersRegisterReq struct {
-	g.Meta   `path:"user/register" tags:"注册" method:"post" summary:"用户注册"`
-	UserName string `json:"username" v:"required|length:4,30#请输入账号|账号长度为:min到:max位"`
-	PassWord string `json:"password" v:"required|length:6,30#请输入密码|密码长度不够"`
-	Email    string `json:"email" v:"email"`
-}
-
-type UsersRegisterRes struct {
-	Msg string
-}
