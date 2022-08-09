c19adeae7da9d8e090e81017ba75e8a9d68b582a
doujiao_nb <1129170176@qq.com>
2022-08-09 18:28:29 +0800

    test1

--- api/v1/user.go
+++ api/v1/user.go
@@ -6,23 +6,12 @@ import "github.com/gogf/gf/v2/frame/g"
 用户注册
 */
 type UserRegisterReq struct {
-	g.Meta      `path:"user/register" tags:"注册" method:"post" summary:"用户注册"`
+	g.Meta   `path:"user/register" tags:"注册" method:"post" summary:"用户注册"`
 	UserName string `json:"username" v:"required|length:4,30#请输入账号|账号长度为:min到:max位"`
 	PassWord string `json:"password" v:"required|length:6,30#请输入密码|密码长度不够"`
-	Email string `json:"email" v:"email"`
+	Email    string `json:"email" v:"email"`
 }
 
 type UserRegisterRes struct {
-	Msg string
-}
-
-type UsersRegisterReq struct {
-	g.Meta      `path:"user/register" tags:"注册" method:"post" summary:"用户注册"`
-	UserName string `json:"username" v:"required|length:4,30#请输入账号|账号长度为:min到:max位"`
-	PassWord string `json:"password" v:"required|length:6,30#请输入密码|密码长度不够"`
-	Email string `json:"email" v:"email"`
-}
-
-type UsersRegisterRes struct {
 	Msg string
 }
