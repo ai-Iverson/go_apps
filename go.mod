f2dfd655e9868a366d39fa4f4a557693604bf238
doujiao_nb <1129170176@qq.com>
2022-08-10 16:36:21 +0800

    修改登录接口验证

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module myapp
 
 go 1.15
 
-require github.com/gogf/gf/v2 v2.0.6
+require (
+	github.com/gogf/gf/v2 v2.0.6
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+)
--- internal/service/user.go
+++ internal/service/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
+	"golang.org/x/crypto/bcrypt"
 	v1 "myapp/api/v1"
 )
 
@@ -39,13 +40,31 @@ func (s *sUser) ValidRegist(ctx context.Context, username string, mail string) e
 }
 
 func (s *sUser) AddUserToDb(ctx context.Context, UserName, Email, PassWord string) (*v1.UserRegisterRes, error) {
+	encryptPassword, _ := bcrypt.GenerateFromPassword([]byte(PassWord), 10)
 	_, err := g.Model("User").Data(g.Map{
-		"username": UserName,
-		"email":    Email,
-		"password": PassWord,
+		"username":   UserName,
+		"email":      Email,
+		"password":   encryptPassword,
+		"is_deleted": 0,
+		"active":     1,
 	}).Insert()
 	if err != nil {
 		return nil, err
 	}
 	return nil, err
+}
+func (s *sUser) CheckUserPassword(ctx context.Context, username string, password string) error {
+	record, err := g.Model("user").Where(g.Map{
+		"username":   username,
+		"password":   password,
+		"is_deleted": 0,
+	}).One()
+	if err != nil && g.IsNil(record) {
+		return err
+	}
+	err1 := bcrypt.CompareHashAndPassword([]byte(record["password"].String()), []byte(password))
+	if err1 != nil {
+		return err1
+	}
+	return nil
 }
